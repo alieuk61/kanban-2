6f96feac4d0ecb5fc484d36651fd662bdba833a1
== <==>
2026-02-22 23:47:19 +0000

    feat(api): shape board data into a nested structure

--- lib/queries/boards.sql
+++ lib/queries/boards.sql
@@ -1,20 +1,29 @@
 -- get all boards
--- get boards
-    -- SELECT * FROM boards
-    -- WHERE id = $1;
+    SELECT * FROM boards
+    WHERE id = $1;
 
 --  get columns
-    -- SELECT * FROM columns
-    -- WHERE board_id = $1
-    -- ORDER BY position;
+    SELECT * FROM columns
+    WHERE board_id = $1
+    ORDER BY position;
 
 
--- get tasks
-    SELECT * FROM tasks
-    join subtasks on tasks.id = subtasks.task_id
-
--- get subtasks
-    -- SELECT * FROM subtasks
+-- get tasks and subtasks
+    SELECT
+    c.id AS column_id,
+    t.id AS task_id,
+    t.title,
+    t.description,
+    t.position AS task_position,
+    s.id AS subtask_id,
+    s.title AS subtask_title,
+    s.is_done,
+    s.position AS subtask_position
+    FROM columns c
+    LEFT JOIN tasks t ON t.column_id = c.id
+    LEFT JOIN subtasks s ON s.task_id = t.id
+    WHERE c.board_id = $1
+    ORDER BY c.position, t.position, s.position
 
 -- create board
 -- INSERT INTO boards(name)
