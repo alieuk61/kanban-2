50c4f9953168bec8686864d63046da1a0086938a
== <==>
2026-05-03 03:37:21 +0100

    refactor(tasks): store column tasks in local state

--- components/cards/column-card.tsx
+++ components/cards/column-card.tsx
@@ -1,36 +1,39 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useAppContext } from "@/context/kanban-context";
 import TaskCard from "./task-card";
-
-type Column = {
-    id: number;
-    name: string;
-    tasks?: Task[];
-};
-
-type Task = {
-    id: number;
-    title: string;
-};
+import type { Column, Task } from "@/types/types";
 
 export default function ColumnCard({ column }: { column: Column }){
 
-    const {tasks} = useAppContext();
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const { getTasksByColumn, chosenBoardId } = useAppContext();
+
+    useEffect(() => {
+        async function loadTasks() {
+            if (!chosenBoardId) return;
+
+            const data = await getTasksByColumn(Number(chosenBoardId), Number(column.id));
+            setTasks(data);
+        }
+
+        loadTasks();
+    }, [chosenBoardId, column.id]);
 
 
     return (
-        <div className="min-w-72 rounded-md p-4 text-black">
+        <div className="min-w-72 rounded-md p-4 text-black mr-2">
             <h2 className="mb-4 font-bold ">
                 {column.name}
             </h2>
 
-            {tasks && tasks.length > 0 ? tasks.map((t) => {
+            {tasks.length > 0 ? tasks.map((t) => {
+                console.log('tasks: ', tasks)
                 return(
                     <div>
                         <TaskCard key={t.id} task={t} />
                     </div>
                 )
-            })}
+            }) : null };
 
 
         </div>
