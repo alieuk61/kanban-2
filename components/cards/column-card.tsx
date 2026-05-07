f26d85f76e1df0a29bbcf4db96810049ec617711
== <==>
2026-05-07 06:52:34 +0100

    refactor(readability): clean up code  and add task toggling function to event listener

--- components/cards/column-card.tsx
+++ components/cards/column-card.tsx
@@ -1,10 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppContext } from "@/context/kanban-context";
 import TaskCard from "./task-card";
 import type { Column, Task } from "@/types/types";
 
-export default function ColumnCard({ column }: { column: Column }){
-
+export default function ColumnCard({ column, onTaskClick }: { column: Column, onTaskClick: (task: Task) => void }) {
     const [tasks, setTasks] = useState<Task[]>([]);
     const { getTasksByColumn, chosenBoardId } = useAppContext();
 
@@ -12,29 +11,30 @@ export default function ColumnCard({ column }: { column: Column }){
         async function loadTasks() {
             if (!chosenBoardId) return;
 
-            const data = await getTasksByColumn(Number(chosenBoardId), Number(column.id));
+            const data = await getTasksByColumn(
+                Number(chosenBoardId),
+                Number(column.id)
+            );
             setTasks(data);
         }
 
         loadTasks();
     }, [chosenBoardId, column.id]);
 
-
     return (
         <div className="min-w-72 rounded-md p-4 text-black mr-2">
-            <h2 className="mb-4 font-bold ">
-                {column.name}
-            </h2>
+            <h2 className="mb-4 font-bold">{column.name}</h2>
 
-            {tasks.length > 0 ? tasks.map((t) => {
-                console.log('tasks: ', tasks)
-                return(
-                    <div>
-                        <TaskCard key={t.id} task={t} />
+            {tasks.length > 0
+                ? tasks.map((t) => (
+                    <div key={t.id}>
+                        <TaskCard
+                            task={t}
+                            onClick={() => onTaskClick(t)}
+                        />
                     </div>
-                )
-            }) : null };
-
+                ))
+                : null}
 
         </div>
     );
