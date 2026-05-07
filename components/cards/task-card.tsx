b77f596c618dc7744c64c4194a5d77488c9d4f05
== <==>
2026-05-07 06:56:21 +0100

    feat(event listener): add onclick function, props and types

--- components/cards/task-card.tsx
+++ components/cards/task-card.tsx
@@ -1,9 +1,17 @@
 import { Task } from "@/types/types";
 import { useState } from "react";
 
-export default function TaskCard({ task }: { task: Task[] }) {
+export default function TaskCard({
+  task,
+  onClick,
+}: {
+  task: Task;
+  onClick: () => void;
+}) {
     return(
-        <div className="bg-white p-2 rounded-lg flex flex-col cursor-pointer ">
+        <div 
+        onClick={onClick}
+        className="bg-white p-2 rounded-lg flex flex-col cursor-pointer ">
             <h2>{task.title}</h2>
             <p>completed subtasks out of subtask length</p>
         </div>
